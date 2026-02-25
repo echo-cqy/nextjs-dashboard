3aabcff36c955b9eea9e935693b0e77f083dc8c5
echoCqy <echoCqy@gmail.com>
2026-02-25 18:48:10 +0800

    feat: 更新发票页面和相关组件，移除 .env.example 文件

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Page() {
+
+  
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
